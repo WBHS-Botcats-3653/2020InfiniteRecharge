3d429c270738b907738fc1c18650289bf3586bf4
fshcat <jhoyos314@gmail.com>
2020-07-20 13:29:00 -1000

    Commented RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,9 +38,13 @@ public class RobotContainer {
   
   public int climbMode = 0;
 
+  // Initiates options-list for the auto command
   private SendableChooser<Command> autoCommand = new SendableChooser();
+
+  // Initiates camera object
   private UsbCamera cam0 = null;
 
+  // Creates instances for every subsystem and the controller
   private final climb m_climb = new climb();
   private final drive m_drive = new drive();
   private final outtake m_out = new outtake();
@@ -54,10 +58,14 @@ public class RobotContainer {
   private final digitalinputs m_digital = new digitalinputs();
   private final XboxController m_controller = new XboxController(0);
   private  final ultrasonic m_ultra = new ultrasonic();
-   /* The container for the robot.  Contains subsystems, OI devices, and commands.
-   */
+
   public RobotContainer() {
-    //autoCommand.addOption("Move past line", new autoDrive(m_drive,m_drivesensors,-8));
+
+    // Creates the different auto command options.
+    // First option drives straight off the init-
+    // iation line, second aims and shoots 3 balls
+    // first. Second is the default.
+    autoCommand.addOption("Move past line", new autoDrive(m_drive,m_drivesensors,-8));
     autoCommand.setDefaultOption("Shoot 3, move past line", 
     new ParallelCommandGroup(
       new SequentialCommandGroup(
@@ -74,16 +82,21 @@ public class RobotContainer {
     ));
     SmartDashboard.putData("Auto Command",autoCommand);
     
-    
+    // Starts camera and configures resolution/fps
     cam0 = CameraServer.getInstance().startAutomaticCapture(0);
 		cam0.setResolution(72, 45);
 		cam0.setFPS(20);
 
-    // Configure the button bindings
+    // Sets default drive command. Lambdas are used 
+    // to pass analog inputs, arithmetic calculates
+    // changed speed from climbMode value. 
     m_drive.setDefaultCommand(
-        new differentialDrive(m_drive, () -> m_controller.getY(GenericHID.Hand.kLeft)*((1-m_climbMode.climbMode)*0.25+0.75), () -> m_controller.getX(GenericHID.Hand.kRight)*((1-m_climbMode.climbMode)*0.25+0.75)));
-        //new testControllerInputs(m_controller,m_drive));
-        configureButtonBindings();
+        new differentialDrive(m_drive, 
+                              () -> m_controller.getY(GenericHID.Hand.kLeft)*((1-m_climbMode.climbMode)*0.25+0.75), 
+                              () -> m_controller.getX(GenericHID.Hand.kRight)*((1-m_climbMode.climbMode)*0.25+0.75))
+        );
+
+    configureButtonBindings();
   }
 
   /**
@@ -94,13 +107,30 @@ public class RobotContainer {
    */
 
   private void configureButtonBindings() {
+    // Trigger deadzone
     double trigEpsilon = 0.3;
+    
+    // Binds all buttons to trigger instances. Trigger
+    // constructor takes a boolean function as a para-
+    // meter. Internally it calls this function, and the
+    // value returned determines whether to call the
+    // supplied command. Usage of :: takes a boolean
+    // method from the controller instance.
     Trigger buttonA = new Trigger(m_controller::getAButton);
     Trigger buttonB = new Trigger(m_controller::getBButton);
     Trigger buttonX = new Trigger(m_controller::getXButton);
     Trigger buttonY = new Trigger(m_controller::getYButton);
+    
+    // Here the numeric values of the bumpers are used
+    // to create JoystickButton instances, which act nearly
+    // the same as Triggers.
     JoystickButton leftBump = new JoystickButton(m_controller,5);
     JoystickButton rightBump = new JoystickButton(m_controller,6);
+    
+    // Controller triggers (not the object, but the button)
+    // are analog, but are being used here as digital inputs.
+    // The conversion is made using lambdas and the previously
+    // established deadzone trigEpsilon
     Trigger rightTrig = new Trigger(
       () -> {
              return (m_controller.getTriggerAxis(GenericHID.Hand.kRight)>trigEpsilon);
@@ -111,8 +141,13 @@ public class RobotContainer {
               return (m_controller.getTriggerAxis(GenericHID.Hand.kLeft)>trigEpsilon);
             }
     );
+
     Trigger backButton = new Trigger(m_controller::getBackButton);
     Trigger startButton = new Trigger(m_controller::getStartButton);
+    
+    // Here the angle values corresponding
+    // to each d-pad input are established,
+    // again using lambdas.
     Trigger bottomDPad = new Trigger(
       () -> {
               return (m_controller.getPOV(0)>135 && m_controller.getPOV(0)<225);
@@ -133,9 +168,11 @@ public class RobotContainer {
               return (m_controller.getPOV(0)>=315 || (m_controller.getPOV(0)<=45 && m_controller.getPOV(0)>0));
             }
     );
+
+    // Creates trigger for retrieving
+    // drive mode
     Trigger climbMode = new Trigger(
       () -> {
-              //m_controller.setRumble(RumbleType.kLeftRumble,1);
               if(m_climbMode.climbMode == 1){
                 return true;
               } else {
@@ -143,7 +180,10 @@ public class RobotContainer {
               }
             }
     );
-    
+
+    // Switches between regular driving
+    // and climb mode. When a switch is
+    // made, controller quickly rumbles.
     backButton.whenActive(
       new SequentialCommandGroup(
         new toggleClimbMode(m_climbMode),
@@ -151,9 +191,12 @@ public class RobotContainer {
         new WaitCommand(0.5),
         new rumble(m_controller,false)
         ));
-    //buttonA.whileActiveContinuous(new intakeEngage(m_in,1));
     
-    //(climbMode.negate()).and(topDPad).toggleWhenActive(new controlLift(m_control,Constants.servoTheta1));
+    // Binds each trigger to a command.
+    // Separated into climbMode and non-
+    // climbMode bindings. Y button being
+    // held is generally used to reverse 
+    // the direction of a button's action
     (climbMode.negate()).and(bottomDPad).toggleWhenActive(new controlLift(m_control,Constants.servoTheta1));
     (climbMode.negate()).and(leftDPad).whileActiveOnce(new controlSpin(m_control,-1));
     (climbMode.negate()).and(rightDPad).whileActiveOnce(new controlSpin(m_control,1));
@@ -168,21 +211,18 @@ public class RobotContainer {
     (climbMode.negate()).and(leftTrig).whileActiveOnce(new autoAim(m_lime,m_drive,() -> m_controller.getY(GenericHID.Hand.kLeft)));
     //(climbMode.negate()).and(rightBump).whenActive(new changePipeline(m_lime));
     (climbMode.negate()).and(leftBump).whenActive(new changeMode(m_lime));
-
-    climbMode.and(buttonX.or(buttonB)).whileActiveOnce(new winchEngage(m_climb,1,() -> m_controller.getXButton(),() -> m_controller.getBButton()));
+    
+    // Climb mode mindings
+    climbMode.and(buttonX.or(buttonB)).whileActiveOnce(new winchEngage(m_climb,1,
+          () -> m_controller.getXButton(),
+          () -> m_controller.getBButton()
+          ));
     climbMode.and(rightTrig.or(leftTrig)).whileActiveOnce(new climbEngage(m_climb,-1,
-    () -> { return (m_controller.getTriggerAxis(GenericHID.Hand.kLeft)>trigEpsilon); },
-    () -> { return (m_controller.getTriggerAxis(GenericHID.Hand.kRight)>trigEpsilon); }));
+          () -> { return (m_controller.getTriggerAxis(GenericHID.Hand.kLeft)>trigEpsilon); },
+          () -> { return (m_controller.getTriggerAxis(GenericHID.Hand.kRight)>trigEpsilon); }
+          ));
     climbMode.and(rightBump.or(leftBump)).whileActiveOnce(new climbEngage(m_climb,1,() -> m_controller.getBumper(GenericHID.Hand.kLeft),() -> m_controller.getBumper(GenericHID.Hand.kRight)));
     climbMode.and(buttonY.and(buttonX.or(buttonB))).whileActiveOnce(new winchEngage(m_climb,-1,() -> m_controller.getXButton(),() -> m_controller.getBButton()));
-    /*
-    rightTrig.whileActiveOnce(new ParallelCommandGroup(new intakeEngage(m_in,1), new storageEngage(m_store)));
-    leftTrig.whileActiveOnce(new ParallelCommandGroup(new deliveryEngage(m_con,1), new outtakeEngage(m_out,1)));
-    leftTrig.and(rightBump).whileActiveOnce(new ParallelCommandGroup(new deliveryEngage(m_con,-1), new outtakeEngage(m_out,-1)));
-    (buttonA.or(buttonB)).and(rightBump.negate()).whileActiveOnce(new climbEngage(m_climb,1,() -> m_controller.getAButton(),() -> m_controller.getBButton()));
-    buttonY.or(buttonX).whileActiveOnce(new winchEngage(m_climb,1,() -> m_controller.getXButton(),() -> m_controller.getYButton()));
-    (buttonA.or(buttonB)).and(rightBump).whileActiveOnce(new climbEngage(m_climb,-1,() -> m_controller.getAButton(),() -> m_controller.getBButton()));
-    */
 
   }
 
